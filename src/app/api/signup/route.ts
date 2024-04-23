b5b5ef58478085fcd4a85085534fa28dd830ce6c
Zaeem Ahmad <60064010+zaeem9236@users.noreply.github.com>
2024-04-24 03:45:30 +0500

    signup API completed

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,18 +1,51 @@
 import dbConnect from "@/lib/dbConnect";
-import { UserModel } from "@/model/User.model";
+import { UserModel, UserInterface } from "@/model/User.model";
 import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/emailTemplates/sendVerificationEmail";
 import { request } from "http";
 import { ApiResponse } from "@/types/ApiResponse";
 
 export const POST = async (req: Request, res: Response): Promise<any> => {
-  await dbConnect()
+  await dbConnect();
   try {
-    const { username, email, passwod } = await req.json();
-    const existingUser = await UserModel.findOne({
+    const { username, email, password } = await req.json();
+
+    const existingUserByUsername: any = await UserModel.findOne({
       username,
-      isVerified: true,
     });
+    const existingUserByEmail: any = await UserModel.findOne({
+      email,
+    });
+    console.log(existingUserByEmail, existingUserByUsername);
+    if (existingUserByUsername?.isVerified || existingUserByEmail?.isVerified) {
+      return Response.json(
+        {
+          success: false,
+          message: "username or email already exists",
+        },
+        { status: 400 }
+      );
+    } else {
+      console.log("else block");
+      const otp = Math.floor(100000 + Math.random() * 900000).toString();
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode: otp,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newUser.save();
+      await sendVerificationEmail({email, username, otp})
+    }
+
     return Response.json(
       { success: true, message: "user registered successfully" },
       {
